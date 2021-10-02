14c3964be40eec69460e469d6a43bba24950bb09
Nicoletr <nicole.trpevski@gmail.com>
2021-10-02 16:35:34 +0800

    Small changes on JS

--- assets/script/search-results.js
+++ assets/script/search-results.js
@@ -4,61 +4,69 @@ var currentDay = document.getElementById("current-day");
 var fiveDay = document.getElementById("5day-results");
 var searchFormEl = document.getElementById("search-form");
 var city = document.getElementById("city-search");
+var currentTemp;
+var currentWindSpeed;
+var currentHumidity;
+var currentUVIndex;
+
 
 //Gets the search value from local storage
 var cityName = localStorage.getItem("inputValue");
 
 //Populates the search input
 city.value = cityName;
-console.log (cityName);
 
-var cityCodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIKey}`
-// var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&appid=${APIKey}`;
+//Function to search the weather API
+function searchWeather(){
 
+    var cityCodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIKey}`;
 
-fetch(cityCodeUrl)
-    .then(function(response) {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].lat);
-            console.log(data[i].lon);
+    fetch(cityCodeUrl)
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var lat = data[i].lat;
+                var lon = data[i].lon;
+            }
+            return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`);
+        })
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-            var lat = data[i].lat;
-            var lon = data[i].lon;
-        }
+            console.log(data.current.temp);
+            console.log(data.current.wind_speed);
+            console.log(data.current.humidity);
+            console.log(data.current.uvi);
 
-        console.log(lat);
-        console.log(lon);
-
-        return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`);
-    })
-    .then(function (response) {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+            // currentTemp = data.current.temp;
+            // currentWindSpeed = data.current.wind_speed;
+            // currentHumidity = data.current.humidity;
+            // currentUVIndex = data.current.uvi;
+        })
+    };
 ;
 
+// currentTemp = data.current.temp;
+// currentWindSpeed = data.current.wind_speed;
+// currentHumidity = data.current.humidity;
+// currentUVIndex = data.current.uvi;
 
-// fetch(url)
-//     .then(function(response) {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data)
-// ;    });
+searchWeather();
+
+console.log(currentTemp);
+console.log(currentWindSpeed);
+console.log(currentHumidity);
+console.log(currentUVIndex);
+
+// TODO: Create function to print data to 
